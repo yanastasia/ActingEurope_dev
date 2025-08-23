c23185a4c35b5307b3065f366c490cfd55ad3310
yanastasia <yakimovskaanastasia@gmail.com>
2025-08-23 20:54:42 +0200

    fix security

--- lib/prisma-client/runtime/library.d.ts
+++ lib/prisma-client/runtime/library.d.ts
@@ -1180,10 +1180,6 @@ declare type Error_2 = {
         foreignKey: {};
     };
 } | {
-    kind: 'DatabaseNotReachable';
-    host?: string;
-    port?: number;
-} | {
     kind: 'DatabaseDoesNotExist';
     db?: string;
 } | {
@@ -1192,11 +1188,6 @@ declare type Error_2 = {
 } | {
     kind: 'DatabaseAccessDenied';
     db?: string;
-} | {
-    kind: 'ConnectionClosed';
-} | {
-    kind: 'TlsConnectionError';
-    reason: string;
 } | {
     kind: 'AuthenticationFailed';
     user?: string;
@@ -2420,7 +2411,7 @@ export declare const objectEnumValues: {
     };
 };
 
-declare const officialPrismaAdapters: readonly ["@prisma/adapter-planetscale", "@prisma/adapter-neon", "@prisma/adapter-libsql", "@prisma/adapter-better-sqlite3", "@prisma/adapter-d1", "@prisma/adapter-pg", "@prisma/adapter-mssql", "@prisma/adapter-mariadb"];
+declare const officialPrismaAdapters: readonly ["@prisma/adapter-planetscale", "@prisma/adapter-neon", "@prisma/adapter-libsql", "@prisma/adapter-d1", "@prisma/adapter-pg", "@prisma/adapter-mssql"];
 
 export declare type Omission = Record<string, boolean | Skip>;
 
@@ -2738,7 +2729,6 @@ declare interface Queryable<Query, Result> extends AdapterInfo {
 declare type QueryCompiler = {
     compile(request: string): {};
     compileBatch(batchRequest: string): BatchResponse;
-    free(): void;
 };
 
 declare interface QueryCompilerConstructor {
@@ -2778,11 +2768,6 @@ declare interface QueryEngineConstructor {
 declare type QueryEngineInstance = {
     connect(headers: string, requestId: string): Promise<void>;
     disconnect(headers: string, requestId: string): Promise<void>;
-    /**
-     * Frees any resources allocated by the engine's WASM instance. This method is automatically created by WASM bindgen.
-     * Noop for other engines.
-     */
-    free?(): void;
     /**
      * @param requestStr JSON.stringified `QueryEngineRequest | QueryEngineBatchRequest`
      * @param headersStr JSON.stringified `QueryEngineRequestHeaders`
@@ -3087,7 +3072,6 @@ declare type SchemaArg = ReadonlyDeep_2<{
     isNullable: boolean;
     isRequired: boolean;
     inputTypes: InputTypeRef[];
-    requiresOtherFields?: string[];
     deprecation?: Deprecation;
 }>;
 
